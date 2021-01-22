1f906b409a6815d7fce001647a55b422986b6928
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2021-01-22 15:25:04 -0300

    Finalizando o Sistema de Cadastro

--- app/views/cliente/Home.php
+++ app/views/cliente/Home.php
@@ -2,22 +2,22 @@
                 <h1 class="titulo">Seja Bem Vindo</h1>
                 <div class="base-colunas">
                     <div>
-                        <a href="frm_cadastro.html" class="col">
+                        <a href="<?php echo URL_BASE . "cliente/novo" ?>" class="col">
                             <i class="icone ico1"></i>
                             <span><b>Novo</b> Cadastro</span>
                         </a>
 
-                        <a href="lst_cliente.html" class="col">
+                        <a href="<?php echo URL_BASE . "cliente/index" ?>" class="col">
                             <i class="icone ico2"></i>
                             <span><b>Lista</b> de Contatos</span>
                         </a>
 
-                        <a href="frm_cadastro.html" class="col">
+                        <a href="<?php echo URL_BASE . "cliente/index" ?>" class="col">
                             <i class="icone ico3"></i>
                             <span><b>Editar</b> Contatos</span>
                         </a>
 
-                        <a href="frm_cadastro.html" class="col">
+                        <a href="<?php echo URL_BASE . "cliente/index" ?>" class="col">
                             <i class="icone ico4"></i>
                             <span><b>Excluir</b> Contatos</span>
                         </a>
